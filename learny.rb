05b6b2d3d1438e016c0a08b29950c54f7f2e1cfc
Drew Bomhof <drew@twowaysoftware.com>
2025-05-21 15:55:12 -0400

    Added all the new stuff

---
+++ learny.rb
@@ -0,0 +1,9 @@
+require "ruby_llm"
+
+RubyLLM.configure do |config|
+  config.anthropic_api_key = ENV.fetch("ANTHROPIC_API_KEY", nil)
+  config.default_model = "claude-3-7-sonnet"
+end
+
+require_relative "src/agent"
+Agent.new.run
